79cf0bd269f36a3ddae935d771fd74bb96a07417
snndmr <snndmrprx@gmail.com>
2020-06-18 02:59:27 +0300

    shape detection func added

--- Operations.cpp
+++ Operations.cpp
@@ -1,61 +1,5 @@
 #include "Operations.h"
 
-Operations::Operations() {}
-
-Operations::~Operations() {}
-
-bool Operations::capture(const char *camId) {
-	VideoCapture capture(camId);
-
-	if(!capture.isOpened()) {
-		cout << format("\n %s could not be opened\n", camId);
-		return EXIT_FAILURE;
-	}
-
-	char pressedKey;
-	bool isPaused = false;
-
-	vector<vector<Point>> approx;
-	vector<vector<Point>> contours;
-
-	Mat frame;
-	while(true) {
-		pressedKey = waitKey(1);
-
-		if(pressedKey == ' ') {
-			isPaused = !isPaused;
-		}
-
-		if(isPaused) {
-			continue;
-		}
-
-		capture.read(frame);
-		if(frame.empty()) break;
-
-		Mat pre = findColor(frame);
-
-		float area = (float) sum(frame)[0];
-		if(area > 1e5) {
-			findContours(pre, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-			approx.resize(contours.size());
-
-			for(int i = 0; i < contours.size(); i++) {
-				drawContours(frame, contours, i, Scalar(0, 255, 255), 5);
-
-				double length = arcLength(contours[i], true);
-				approxPolyDP(contours[i], approx[i], 0.04 * length, true);
-
-				RotatedRect rect = minAreaRect(contours[i]);
-				putText(frame, format("%s", approx[i].size() == 4 ? "Rect" : "Undefined"),
-						Point2f(rect.center.x, rect.center.y), FONT_HERSHEY_DUPLEX, 2, Scalar(0, 0, 255), 2);
-			}
-		}
-		imshow(WINDOW_NAME_MAIN, frame);
-	}
-	return EXIT_SUCCESS;
-}
-
 Mat Operations::findColor(Mat image) {
 	Mat hsv, mask, output;
 
@@ -66,4 +10,54 @@ Mat Operations::findColor(Mat image) {
 	cvtColor(output, output, COLOR_BGR2GRAY);
 	medianBlur(output, output, 3);
 	return output;
+}
+
+void Operations::findShape(Mat frame, vector<Point> &contour, vector<Point> &approx) {
+	double length = arcLength(contour, true);
+	approxPolyDP(contour, approx, 0.04 * length, true);
+
+	RotatedRect rect = minAreaRect(contour);
+	putText(frame, format(". %s", approx.size() == 4 ? "Rect" : "N/A"),
+			Point2f(rect.center.x, rect.center.y), FONT_HERSHEY_DUPLEX, 1.8, Scalar(0, 155, 255), 3);
+}
+
+bool Operations::capture(const char *camId) {
+	VideoCapture capture(camId);
+
+	if(!capture.isOpened()) {
+		cout << format("\n %s could not be opened\n", camId);
+		return EXIT_FAILURE;
+	}
+
+	char key;
+	bool isPaused = false;
+
+	Mat frame;
+	vector<vector<Point>> approx;
+	vector<vector<Point>> contours;
+
+	do {
+		key = waitKey(1);
+
+		if(key == ' ') { isPaused = !isPaused; }
+		if(isPaused) { continue; }
+
+		capture.read(frame);
+		if(frame.empty()) { break; }
+
+		Mat pre = findColor(frame);
+
+		if(sum(frame)[0] > MIN_AREA) {
+			findContours(pre, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+			approx.resize(contours.size());
+
+			for(int i = 0; i < contours.size(); i++) {
+				drawContours(frame, contours, i, Scalar(0, 255, 155), 3);
+				findShape(frame, contours[i], approx[i]);
+			}
+		}
+		imshow(WINDOW_NAME_PREP, pre);
+		imshow(WINDOW_NAME_MAIN, frame);
+	} while(key != 27);
+	return EXIT_SUCCESS;
 }
--- Operations.h
+++ Operations.h
@@ -9,17 +9,18 @@ using namespace std;
 
 class Operations {
 	public:
-	Operations();
-	~Operations();
 	bool capture(const char *);
 
 	private:
+	const float MIN_AREA = 1e5;
+
 	const char *WINDOW_NAME_MAIN = "Window Main";
 	const char *WINDOW_NAME_PREP = "Window Prep";
 
-	const Scalar lowerBoundBlue = Scalar(80, 150, 150);
-	const Scalar upperBoundBlue = Scalar(120, 255, 255);
+	const Scalar lowerBoundBlue = Scalar( 90, 155, 155);
+	const Scalar upperBoundBlue = Scalar(130, 255, 255);
 
 	Mat findColor(Mat);
+	void findShape(Mat, vector<Point> &, vector<Point> &);
 };
 
