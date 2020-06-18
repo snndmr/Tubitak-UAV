a8804f0e35088f97880b36d451209fb6b215274e
snndmr <snndmrprx@gmail.com>
2020-06-19 02:28:36 +0300

    Operation.cpp updated

--- Operation.cpp
+++ Operation.cpp
@@ -8,8 +8,7 @@ Operation::Operation(const char *camId) {
 		bool isPaused = false;
 
 		Mat frame;
-		while(true) {
-			key = waitKey(1);
+		for(int frameNo = 0; key = waitKey(1); frameNo += 1) {
 			if(key == 27) { break; }
 			if(key == 32) { isPaused = !isPaused; }
 
@@ -20,10 +19,10 @@ Operation::Operation(const char *camId) {
 				process(frame);
 				double dur = clock() - start;
 
-				putText(frame, format("FPS: %.2lf | Avarage Time Per Frame: %.2lf ms.", fps.averagefps(), fps.averagedur(dur)),
-						Point2f(10, 30), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 255, 255), 2);
+				putText(frame, format("FPS : %.2lf | Avg. Time Per Frame : %.2lf ms. | Frame No : %04d", fps.averagefps(), fps.averagedur(dur), frameNo),
+						Point(10, 30), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 155, 255), 2);
 			} else {
-				putText(frame, format("Paused"), Point2f(10, 90), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 255, 255), 2);
+				putText(frame, format("Paused"), Point2f(10, 90), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 155, 255), 2);
 			}
 			imshow(WINDOW_NAME_MAIN, frame);
 		}
@@ -46,15 +45,16 @@ void Operation::process(Mat &frame) {
 	approx.resize(contours.size());
 	for(int i = 0; i < contours.size(); i++) {
 		if(contourArea(contours[i]) > MIN_AREA) {
-			drawContours(frame, contours, i, Scalar(0, 255, 100), 2);
+			drawContours(frame, contours, i, Scalar(0, 255, 255), 1);
 			findShape(frame, contours[i], approx[i]);
 		}
 	}
-	putText(frame, format("Number of Objects Detected: %d", approx.size()),
-			Point2f(10, 60), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0, 255, 255), 2);
+	putText(frame, format("Number of Objects Detected : %d", approx.size()),
+			Point(10, 60), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 155, 255), 2);
 }
 
-Mat Operation::findColor(Mat image, const Scalar &lowerBoundRed, const Scalar &upperBoundRed,
+Mat Operation::findColor(Mat image,
+						 const Scalar &lowerBoundRed, const Scalar &upperBoundRed,
 						 const Scalar &lowerBoundBlue, const Scalar &upperBoundBlue) {
 	Mat hsv, maskRed, maskBlue, output;
 
@@ -74,5 +74,5 @@ void Operation::findShape(Mat frame, vector<Point> &contour, vector<Point> &appr
 
 	RotatedRect rect = minAreaRect(contour);
 	putText(frame, format(". %s", approx.size() == 4 ? "Rectangle" : approx.size() > 5 ? "Circle" : "UGO"),
-			Point2f(rect.center.x, rect.center.y), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(0, 100, 255), 2);
+			Point2f(rect.center.x, rect.center.y), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0, 100, 255), 2);
 }
