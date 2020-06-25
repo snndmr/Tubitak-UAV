b884f1adcfc2f445db98a82ee794eda2920f7d0d
SNN <53225314+snndmr@users.noreply.github.com>
2020-06-26 00:26:04 +0300

    Some minor changes and delete operation added

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,16 @@
 #include "Operation.h"
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char *argv[])
+{
 	VideoCapture capture(argv[1]);
 
-	if(!capture.isOpened()) {
-		cout << format("\n %s could not be opened!", argv[1]);
+	if (!capture.isOpened())
+	{
+		cout << format("\n %s could not be opened!\n", argv[1]);
 		return EXIT_FAILURE;
 	}
 
-	cout << format("\n Capture was initiated in %s", argv[1]);
-	Operation operation(capture);
+	Operation *operation = new Operation(capture);
+	delete operation;
 	return EXIT_SUCCESS;
 }
