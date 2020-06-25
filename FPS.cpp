3a68a8531c9fed475485730f1950ed1f6788a555
SNN <53225314+snndmr@users.noreply.github.com>
2020-06-26 00:22:24 +0300

    Changed to work with Linux - clock() -> mClock()

--- FPS.cpp
+++ FPS.cpp
@@ -1,10 +1,26 @@
 #include "FPS.h"
 
-FPS::FPS() : avarageDuration(0), avarageFps(0), fps1Sec(0), fpsStart(0) {}
+FPS::FPS() : avarageDuration(0), avarageFps(0), fps1Sec(0), fpsStart(0)
+{
+	cout << "\n FPS counter was started.";
+}
 
-double FPS::calcAvgFps() {
-	if(clock() - fpsStart >= 1000) {
-		fpsStart = clock();
+FPS::~FPS()
+{
+	cout << "\n FPS counter is finished.";
+}
+
+double FPS::mClock()
+{
+	clock_gettime(CLOCK_MONOTONIC, &mTimeSpec);
+	return (mTimeSpec.tv_sec * 1000) + (mTimeSpec.tv_nsec * 1e-6);
+}
+
+double FPS::calcAvgFps()
+{
+	if (mClock() - fpsStart >= 1000)
+	{
+		fpsStart = mClock();
 		avarageFps = 0.7 * avarageFps + 0.3 * fps1Sec;
 		fps1Sec = 0;
 	}
@@ -15,6 +31,3 @@ double FPS::calcAvgFps() {
 double FPS::calcAvgDur(double newdur) { return 0.98 * avarageDuration + 0.02 * newdur; }
 
 double FPS::getAvarageFps() { return avarageFps; }
-
-FPS::~FPS() {}
-
