27fe719e3231d1468ad356cbba8a2b02a4d09dae
SNN <53225314+snndmr@users.noreply.github.com>
2020-07-01 16:33:33 +0300

    opencv installer for raspberry pi

---
+++ cv2rasp.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# Author: snn
+# To use: g++ main.cpp -o run `pkg-config --cflags --libs opencv4` && ./run [argv if there is argument]
+#
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+sudo apt-get autoclean
+
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev pkg-config 
+
+cd ~/ && git clone https://github.com/opencv/opencv.git
+cd ~/opencv/ && mkdir build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENCV_GENERATE_PKGCONFIG=ON -DCMAKE_INSTALL_PREFIX=/usr/local ..
+
+make -j4
+sudo make install
+sudo ldconfig
+
+reboot
