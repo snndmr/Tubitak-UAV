3f10f0e8a33e00ac9b4111972ccb05ff59933f6c
SNN <53225314+snndmr@users.noreply.github.com>
2020-07-06 21:06:31 +0300

    Update cv2rasp.sh

--- cv2rasp.sh
+++ cv2rasp.sh
@@ -6,10 +6,15 @@
 # To test: g++ main.cpp -o run `pkg-config --cflags --libs opencv4` && ./run [argv if there is argument]
 #
 
+sudo apt purge sonic-pi -y
+sudo apt purge minecraft-pi -y
+sudo apt purge libreoffice* -y
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get autoremove -y
+sudo apt-get clean -y
 sudo apt-get autoclean -y
+sudo apt-get autoremove -y
 
 sudo apt-get install -y build-essential
 sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev pkg-config 
