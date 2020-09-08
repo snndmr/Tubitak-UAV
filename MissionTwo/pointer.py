95f8fbe9acff002d9e34b2240c02f004fe5682c5
snn <snndmrprx@gmail.com>
2020-09-09 02:52:05 +0300

    Update

--- MissionTwo/pointer.py
+++ MissionTwo/pointer.py
@@ -10,6 +10,9 @@ class Pointer:
     def get_data(self):
         return self.data
 
+    def set_data(self, data):
+        self.data = data
+
     def add_data(self, value):
         self.id += 1
         self.data[self.id] = value
@@ -21,12 +24,18 @@ class Pointer:
     def read_from_file(self):
         if os.path.exists('./data.json'):
             with open('data.json') as json_file:
-                self.data = json.load(json_file)
+                self.set_data(json.load(json_file))
 
-    def show_data(self):
+    def show_specific_data(self, index):
+        print(' Point  : {}'.format(index))
+        print(' --------------')
+        print(' Feature: {}'.format(self.data[index]['Feature']))
+        print(' Alt    : {}'.format(self.data[index]['Alt']))
+        print(' Lat    : {}'.format(self.data[index]['Lat']))
+        print(' Lon    : {}'.format(self.data[index]['Lon']))
+        print(' Speed  : {}'.format(self.data[index]['Speed']))
+        print('')
+
+    def show_all_data(self):
         for index in self.data:
-            print(' Feature: ', self.data[index]['Feature'])
-            print(' Alt: ', self.data[index]['Alt'])
-            print(' Lat: ', self.data[index]['Lat'])
-            print(' Lon: ', self.data[index]['Lon'])
-            print('')
+            self.show_specific_data(index)
