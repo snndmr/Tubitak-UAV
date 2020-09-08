79e34fe8f1bf40859fb992f098ea2676f8f50cf8
snn <snndmrprx@gmail.com>
2020-09-09 02:51:23 +0300

    Blue square find logic removed

--- MissionTwo/detector.py
+++ MissionTwo/detector.py
@@ -2,6 +2,8 @@ import cv2
 
 from fps import FPS
 
+MIN_AREA = 1e3
+
 
 def findRedColor(image, hsv):
     maskRedLeft = cv2.inRange(hsv, (0, 100, 100), (10, 255, 255))
@@ -10,27 +12,14 @@ def findRedColor(image, hsv):
     return cv2.cvtColor(maskedImage, cv2.COLOR_BGR2GRAY)
 
 
-def findBlueColor(image, hsv):
-    maskBlue = cv2.inRange(hsv, (90, 100, 100), (140, 255, 255))
-    maskedImage = cv2.bitwise_and(image, image, mask=maskBlue)
-    return cv2.cvtColor(maskedImage, cv2.COLOR_BGR2GRAY)
-
-
-def isSquare(contour):
-    (width, height) = cv2.minAreaRect(contour)[1]
-    aspectRatio = min(width, height) / max(width, height)
-    approx = cv2.approxPolyDP(contour, 0.04 * cv2.arcLength(contour, True), True)
-    return True if len(approx) == 4 and cv2.isContourConvex(approx) and 0.9 < aspectRatio < 1.1 else False
-
-
 def isCircle(contour):
     approx = cv2.approxPolyDP(contour, 0.02 * cv2.arcLength(contour, True), True)
     return True if len(approx) > 6 and cv2.isContourConvex(approx) else False
 
 
 class Detector:
-    def __init__(self):
-        self.cap = cv2.VideoCapture()
+    def __init__(self, cam_id):
+        self.cap = cv2.VideoCapture(cam_id)
         self.fps = FPS()
 
     @staticmethod
@@ -39,15 +28,11 @@ class Detector:
         hsv = cv2.medianBlur(hsv, 3)
 
         contourRed = cv2.findContours(findRedColor(image, hsv), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-        contourBlue = cv2.findContours(findBlueColor(image, hsv), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
         bundles = list()
 
         for contour in contourRed:
-            if cv2.contourArea(contour) > 1e3 and isCircle(contour):
+            if cv2.contourArea(contour) > MIN_AREA and isCircle(contour):
                 bundles.append(('Circle', contour))
 
-        for contour in contourBlue:
-            if cv2.contourArea(contour) > 1e3 and isSquare(contour):
-                bundles.append(('Square', contour))
         return bundles
